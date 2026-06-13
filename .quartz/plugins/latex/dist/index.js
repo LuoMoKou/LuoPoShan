dc4a923105716f498ba7ac222ecc6e2c839d5044
LuoMoKou <3250101632@zju.edu.cn>
2026-06-13 15:00:04 +0800

    fix: KaTeX CDN 换成国内 BootCDN 镜像

--- .quartz/plugins/latex/dist/index.js
+++ .quartz/plugins/latex/dist/index.js
@@ -233911,10 +233911,10 @@ var Latex = (opts) => {
       switch (engine) {
         case "katex":
           return {
-            css: [{ content: "https://cdn.jsdelivr.net/npm/katex@0.16.11/dist/katex.min.css" }],
+            css: [{ content: "https://cdn.bootcdn.net/ajax/libs/KaTeX/0.16.11/katex.min.css" }],
             js: [
               {
-                src: "https://cdn.jsdelivr.net/npm/katex@0.16.11/dist/contrib/copy-tex.min.js",
+                src: "https://cdn.bootcdn.net/ajax/libs/KaTeX/0.16.11/contrib/copy-tex.min.js",
                 loadTime: "afterDOMReady",
                 contentType: "external"
               }
